84de603f6d332bd7008e33be08efc7f97676315f
Tolga Taner <dev.tolgataner@icloud.com>
2025-03-18 15:22:19 +0300

    WIP: initialize the project

---
+++ OzanSuperApp/OzanSuperApp/OzanSuperAppTests/OzanSuperAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  OzanSuperAppTests.swift
+//  OzanSuperAppTests
+//
+//  Created by Tolga Taner on 18.03.2025.
+//
+
+import Testing
+@testable import OzanSuperApp
+
+struct OzanSuperAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
